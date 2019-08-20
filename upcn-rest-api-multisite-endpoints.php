ca7501b8c7d0d70fe474e8e1d6ee99adfac64f1b
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-08-20 16:06:48 -0300

    endpoint featured posts

--- upcn-rest-api-multisite-endpoints.php
+++ upcn-rest-api-multisite-endpoints.php
@@ -170,8 +170,16 @@ function lnk_get_site_posts(WP_REST_Request $request){
      switch_to_blog($site->blog_id);
 
      $posts_args = array(
-        'numberposts' => $count
-     );
+      'numberposts' => '-1',
+      'meta_query' => array(
+        'relation' => 'OR',
+        array(
+          'key' => 'lnk_onhome',
+          'compare' => '=',
+          'value' => '1'
+        )
+    )
+   );
      $posts = get_posts($posts_args);
 
      foreach($posts as $post_key => $post){
@@ -194,9 +202,64 @@ function lnk_get_site_posts(WP_REST_Request $request){
      restore_current_blog();
    }
    usort($allPosts,'lnk_compare_by_date');
-   $allPosts = array_slice($allPosts,0,$count);
    return new WP_REST_Response($allPosts, 200 );
  }
+
+/**
+ * Lista de posts destacados de todos los sitios
+ *
+ * @param WP_REST_Request $request
+ * @return WP_REST_Response: Lista de posts
+ */
+function lnk_get_sites_featured_posts(WP_REST_Request $request){
+
+  $count = $request->get_param("count");
+
+  $sites_args = array(
+    'public' => 1 // los posts tb solo publicos?
+  );
+  $sites = get_sites($sites_args);
+  $allPosts = array();
+  if(is_array($sites))
+  foreach($sites as $site_key => $site){
+    switch_to_blog($site->blog_id);
+
+    $posts_args = array(
+     'numberposts' => '-1',
+     'meta_query' => array(
+       'relation' => 'OR',
+       array(
+        'key' => 'lnk_featured',
+        'compare' => '=',
+        'value' => '1'
+      )
+   )
+  );
+    $posts = get_posts($posts_args);
+
+    foreach($posts as $post_key => $post){
+      $posts[$post_key]->blog = array(
+        'blog_id' => $site->blog_id,
+        'blog_name' => get_bloginfo('name'),
+        'blog_url' => $site->path
+      );
+
+
+      $terms = wp_get_post_categories($post->ID);
+      if(is_array($terms)){
+        $posts[$post_key]->the_term = get_term($terms[0])->slug;
+      }
+
+      $posts[$post_key]->thumbnail = get_the_post_thumbnail_url($post->ID);
+    }
+
+    $allPosts = array_merge($allPosts,$posts);
+    restore_current_blog();
+  }
+  usort($allPosts,'lnk_compare_by_date');
+  return new WP_REST_Response($allPosts, 200 );
+}
+
 
  /**
   * Compara 2 objetos WP_Post para ordenar decrecientemente
